e9122c706afaac9389f5dae7d50a0afaae378005
anirudh32 <76041493+anirudh32@users.noreply.github.com>
2021-06-06 10:43:58 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>JavaScript and React.js</h1>
+      <p>
+        Shape AI is a best way for beginners to learn the basics of web
+        development using JavaScript and react.js from scratch.Not only this,
+        they were conducting bootcamps on other current trending technologies
+        like machine learning ,deep learning etc..
+      </p>
+    </div>
+  );
+}
+
+export default Note;
